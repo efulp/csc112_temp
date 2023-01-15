229eba856b5758cfa2bcb4e256d7eccb73af388d
efulp <31475571+efulp@users.noreply.github.com>
2023-01-15 09:48:30 -0500

    Initial commit

---
+++ Containers.java
@@ -0,0 +1,14 @@
+/**
+ * This program prompts the user for a number of light bulbs, then
+ * determines the smallest number of full containers for the order
+ *
+ * @author Nirre Pluf {@literal <pluf@wfu.edu>}
+ * @version 0.1, Jan. 15, 2023
+ */
+
+public class Containers {
+    public static void main(String[] args) {
+
+    }
+}
+
